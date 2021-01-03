3d8ac999361fceb8e33db468a6542f0da1a017d5
darmalkhan <d.gulabzoi@gmail.com>
2021-01-03 15:27:06 +0430

    make webpage to pdf

--- resources/views/export.blade.php
+++ resources/views/export.blade.php
@@ -11,18 +11,21 @@
     border:1px solid #ccc;
    }
   </style>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script>
+
  </head>
  <body>
   <br />
   <div class="container"> 
    <h3 align="center">Export Data to Excel in Laravel 8</h3><br />
    <div align="center">
+    <a class="btn btn-primary" id="download">Export to PDF</a>
     <a class="btn btn-primary" href="{{ URL::to('/ExportPdf') }}">Export to PDF</a>
     <a href="{{ url('/Export') }}" class="btn btn-success">Export to Excel</a>
     <a id="excel" class="btn btn-danger">Export to Excel by AJAX</a>
    </div>
    <br />
-   <div class="table-responsive">
+   <div id="invoice" class="table-responsive">
     <table class="table table-striped table-bordered">
      <tr>
       <td>name</td>
@@ -52,6 +55,26 @@ $.ajax({
 });
 })
 
+</script>
+
+<script>
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 1,
+                filename: 'myfile.txt',
+                image: { type: 'jpeg', quality: 1 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+                // jspdf({format: 'a6',unit: 'mm',orientation: [l | p] });
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
 </script>
  </body>
 </html> 
